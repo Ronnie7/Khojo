3290fbafcf15861323388dff1895813797301b5e
Ronnie Kumar <Ronnie7@users.noreply.github.com>
2017-05-06 15:27:02 -0500

    Added some comment

--- src/main/java/com/khojo/services/GooglePlacesImpl.java
+++ src/main/java/com/khojo/services/GooglePlacesImpl.java
@@ -60,6 +60,6 @@ public class GooglePlacesImpl implements GooglePlace {
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         double distance = R * c * 1000; // convert to meters
         distance = Math.pow(distance, 2) + Math.pow(0,2);
-        return  (Math.sqrt(distance)) / 1609.344;
+        return  (Math.sqrt(distance)) / 1609.344; //convert to miles.
     }
 }
